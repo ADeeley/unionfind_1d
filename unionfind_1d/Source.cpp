2d8eea326c7db8d27161cd7ddd5099bcc9cbeb32
Adam Deeley <ad.deeley@gmail.com>
2017-06-09 13:31:42 +0100

    Added an open(int a) function and fixed the overflow issue with open on the grid>

--- unionfind_1d/Source.cpp
+++ unionfind_1d/Source.cpp
@@ -6,16 +6,18 @@ using namespace std;
 /*
 The rows of the grid of size Gridsize-1 are selected first. The columns follow
 	  cols 1-4
-row 1 [][][][]
-row 2 [][][][]
-row 3 [][][][]
-row 4 [][][][]
+row 1 [][][][][]
+row 2 [][][][][]
+row 3 [][][][][]
+row 4 [][][][][]
+row 5 [][][][][]
 
 */
 
 struct Sector {
 	int parent = -1;
 	int ID;
+	bool isOpen = false;
 	Sector( int i ) :
 		ID{ i } {};
 
@@ -49,35 +51,49 @@ public:
 	Grid grid;
 	Percolates( int sz ) :
 		grid{ Grid( sz ) }, gridSize{ sz } {}
-	void unite( int idA, int idB );
-	bool find( int idA, int idB );
-	void print_grid();
 	int get_root( int a );
+	void unite( int a, int b );
+	bool find( int a, int b );
+	void print_grid();
+	void open( int a );
 private:
 	int gridSize;
 };
 
 int Percolates::get_root( int a ) {
-	// returns the root note of sector a
+	// Returns the root note of sector a.
 	while ( !( grid[a]->parent == -1 ) )
 		a = grid[a]->parent;
 
 	return a;
 }
-void Percolates :: unite( int idA, int idB ) {
-	// Unites the sectors with index idA and idB
-	// first find the root sectors of idA and idB
-	idA = get_root( idA );
-	idB = get_root( idB );
-	// If idA and idB do not share a root, then make the root of idB the root of idA
-	if ( !( idA == idB ) )
-		grid[idA]->parent = idB;
+void Percolates :: unite( int a, int b ) {
+	// Unites the sectors with index a and b.
+	// First find the root sectors of a and b. 
+	a = get_root( a );
+	b = get_root( b );
+	// If a and b do not share a root, then make the root of b the root of a.
+	if ( !( a == b ) ) {
+		grid[a]->parent = b;
+		cout << "United " << a << " and " << b << '\n';
+	}
+}
+
+bool Percolates::find( int a, int b ) {
+	// Returns true if a and a share a root node
+	a = get_root( a );
+	b = get_root( b );
+
+	if ( a == b )
+		return true;
+	else
+		return false;
 }
 
 void Percolates::print_grid() {
 	int i = 0;
-	for ( int r = 0; r < gridSize; r++ ) {
-		for ( int c = 0; c < gridSize; c++ ) {
+	for ( int row = 0; row < gridSize; row++ ) {
+		for ( int col = 0; col < gridSize; col++ ) {
 			cout << '[' << grid[i]->ID << "]:" << grid[i]->parent << '\t';
 			i += 1;
 		}
@@ -86,16 +102,27 @@ void Percolates::print_grid() {
 	cout << '\n';
 }
 
-bool Percolates::find( int idA, int idB ) {
-	// Returns true if intA and intB share a root node
-	idA = get_root( idA );
-	idB = get_root( idB );
+void Percolates::open(int a) {
+	// Opens the sector a and unites it with any adjacent sectors.
+	grid[a]->isOpen = true;
 
-	if ( idA == idB )
-		return true;
-	else
-		return false;
+	if ( a>0 ) // left
+		if (!((a)%gridSize == 0 )) // checks for column overflow
+			if ( grid[a-1]->isOpen )
+				unite( a, a - 1 );
 
+	if ( a < gridSize*gridSize - 1 ) // right
+		if (!((a+1)%gridSize == 0 )) // checks for column overflow
+			if ( grid[a + 1]->isOpen )
+				unite( a, a + 1 );
+
+	if ( a - gridSize > 0 ) // up
+		if ( grid[a - gridSize]->isOpen )
+			unite( a, a - gridSize );
+
+	if ( a + gridSize < gridSize*gridSize ) // down
+		if ( grid[a + gridSize]->isOpen )
+			unite( a, a + gridSize );
 }
 
 int main() {
@@ -104,7 +131,6 @@ int main() {
 	/*
 	cout << grd[5]->ID << ',' << grd.get_sector_by_coordinates( 1, 1 )->ID << '\n';
 	cout << grd[14]->ID << ',' << grd.get_sector_by_coordinates( 3,2 )->ID << '\n';
-	*/
 	grd.print_grid();
 	grd.unite( 4, 5 );
 	grd.unite( 5, 6 );
@@ -116,7 +142,22 @@ int main() {
 	cout << grd.find( 4, 8 ) << '\n';
 	cout << grd.find( 4, 11 ) << '\n';
 	cout << grd.find( 7, 9 ) << '\n';
-		
-
+	
+	grd.open( 12 );
+	grd.open( 13 );
+	grd.open( 17 );
+	grd.open( 11 );
+	grd.open( 7 );
+	grd.open( 14 );
+	cout << "grd.open( 19 )\n";
+	grd.open( 19 );
+	cout << "grd.open( 20 )\n";
+	grd.open( 20 );
+	cout << "grd.open( 15 )\n";
+	grd.open( 15 );
+	cout << "grd.open( 10 )\n";
+	grd.open( 10 );
+	grd.print_grid();
+	*/
 	return 0;
 }
